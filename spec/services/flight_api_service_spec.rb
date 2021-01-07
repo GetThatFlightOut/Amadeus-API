0049471382df20325219752c0dfc59d0aadfa070
Connor Ferguson <cpfergus1@gmail.com>
2021-01-06 20:02:01 -0700

    created service testing spec
    Co-authored-by: Aidan Murray 
    <48848183+TeknoServal@users.noreply.github.com>
    Co-authored-by: Brian Liu 
    <67594471+badgerbreezy@users.noreply.github.com>
    Co-authored-by: Connor Ferguson 
    <68167430+cpfergus1@users.noreply.github.com>
    Co-authored-by: George Soderholm 
    <62966396+GeorgieGirl24@users.noreply.github.com>
    Co-authored-by: Jesse Mellinger 
    <29828129+JesseMellinger@users.noreply.github.com>
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>
    Co-authored-by: Sage Freeman-Gonzales 
    <65799246+SageOfCode@users.noreply.github.com>
    Co-authored-by: Sheryl Stillman 
    <7945439+stillsheryl@users.noreply.github.com>
    Co-authored-by: Todd Estes 
    <66448493+Todd-Estes@users.noreply.github.com>
    Co-authored-by: Will Dunlap <53274357+dunlapww@users.noreply.github.com>

--- spec/services/flight_api_service_spec.rb
+++ spec/services/flight_api_service_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'json'
 
 def app
   FlightApiServiceController
@@ -6,7 +7,32 @@ end
 
 describe 'I can connect and recieve a response from the flight API' do
   it "I get JSON data back", :vcr do
-    get '/flights'
-    expect(last_response.body).to be_a(Hash)
+    params = {
+              :fly_from => 'DEN',
+              :date_from => '30/01/2021',
+              :date_to => '30/01/2021',
+              :nights_in_dst_from => 5,
+              :nights_in_dst_to => 5,
+              :flight_type => 'round',
+              :one_for_city => 1,
+              :partner_market => 'us',
+              :curr => 'USD',
+              :sort => 'price',
+              :ret_from_diff_airport => 0,
+              :partner => 'picky',
+              :limit => 20
+              }
+    get '/flights', params
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to be_a(String)
+    response = JSON.parse(last_response.body, symbolize_names: true)
+    expect(response).to be_an(Hash)
+    expect(response[:data]).to be_an(Array)
+    expect(response[:data][0]).to be_an(Hash)
+    expect(response[:data].size).to eq(20)
+    expect(response[:data][0][:price]).to be_an(Integer)
+    expect(response[:data][0][:deep_link]).to_not eq('')
+    expect(response[:data][0][:cityTo]).to be_an(String)
+    expect(response[:data][0][:cityFrom]).to eq('Denver')
   end
 end
